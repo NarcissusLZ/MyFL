e48ee34fa28650b00f34a774628b7d87bb84714b
Narcissus <narcissuslz@qq.com>
2025-09-18 11:51:55 +0800

    update packet_loss

--- client/client.py
+++ client/client.py
@@ -13,8 +13,8 @@ class Client:
         self.local_dataset = local_dataset
         self.gpu_id = gpu_id
         self.device = self._select_device(config['device'])
-        self.distance = id
-        self.packet_loss = self.distance * 0.01+0.01
+        self.distance = id + 1
+        self.packet_loss = self.distance * 0.01
 
         # 通信参数
         self.tx_power = 0.1  # 发射功率 100mW = 0.1W
